86f7a719e98b59170d36501b8ddd454cd902e0f8
David Latorre <david@latorredev.com>
2026-09-17 00:13:37 -0500

    feat/add python standalone version

--- Jenkinsfile
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-@Library('kanolib')
-import build_deb_pkg
-
-
-stage ('Build') {
-    autobuild_repo_pkg 'linux-story'
-}
--- linux_story/strings.py
+++
@@ -1,3 +0,0 @@
-from linux_story.dependencies import translate
-
-COMMAND_BLOCKED_MESSAGE = translate('Nice try! But you do not need that command for this challenge')
